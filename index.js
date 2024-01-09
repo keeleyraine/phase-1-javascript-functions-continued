70b22386c335bef22042e7052626cc0a118ffd09
Keeley Madigan <keeleysprouts@gmail.com>
2024-01-08 20:57:49 -0800

    p1 js functions cont

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun()
+
+let mondayWork = function(action="go to the office") {
+    return `This Monday, I will ${action}.`;
+}
+mondayWork()
+
+function wrapAdjective (style="*") {
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
